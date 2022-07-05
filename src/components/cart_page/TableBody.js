b43e76b2d3766f93d56b0c9192df7d59d757d89d
MHRafix <fmhrafiz27@gmail.com>
2022-07-05 21:06:38 +0600

    fixed:

--- src/components/cart_page/TableBody.js
+++ src/components/cart_page/TableBody.js
@@ -9,7 +9,9 @@ import { handleReduceCart } from "../../utilities/handleReduceCart";
 // import "sweetalert2/src/sweetalert2.scss";
 
 export default function TableBody({ carted_products }) {
-  const Swal = dynamic(() => import("sweetalert2"));
+  const Swal = dynamic(() =>
+    import("sweetalert2").then((res) => console.log(res))
+  );
 
   const { _id, slug, thumbnail, title, prices, quantity } = carted_products;
   const dispatch = useDispatch();
