c895f1944b40dc2b2d1dd82710a3a6c93a132702
Mehedi Hasan Rafiz <86802187+MHRafix@users.noreply.github.com>
2022-07-11 21:14:38 +0600

    Update and rename VerifyUserIdentityMain.js to VerifyUserIdentity.js

--- src/pages/my_account/my_profile/VerifyUserIdentity.js
+++ src/pages/my_account/my_profile/VerifyUserIdentity.js
@@ -2,7 +2,7 @@ import React from "react";
 import LayoutContainer from "../../../components/commons/layout/LayoutContainer";
 import VerifyUserIdentityMain from "../../../components/my_profile_page/edit_account_details_page/VerifyUserIdentityMain";
 
-export default function VerifyUserIdentityMain() {
+export default function VerifyUserIdentity() {
   return (
     <>
       <LayoutContainer
