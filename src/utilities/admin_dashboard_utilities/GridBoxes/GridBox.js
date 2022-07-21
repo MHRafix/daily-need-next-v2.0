b22d7a228e8e2e0532919e430a6ea39d9d93a127
MHRafix <fmhrafiz27@gmail.com>
2022-07-21 16:49:41 +0600

    react coutup and visiblity sensor added for countup number in summury box

--- src/utilities/admin_dashboard_utilities/GridBoxes/GridBox.js
+++ src/utilities/admin_dashboard_utilities/GridBoxes/GridBox.js
@@ -1,4 +1,6 @@
 import React from "react";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
 export default function GridBox({ box_content }) {
   const { box_name, box_number, box_icon, icon_color } = box_content;
@@ -10,7 +12,20 @@ export default function GridBox({ box_content }) {
             <span id="chart_indicator_label">{box_name}</span>
           </div>
           <div>
-            <h1 id="amount_label">{box_number}</h1>
+            <VisibilitySensor partialVisibility style={{ bottom: "200px" }}>
+              {({ isVisible }) => (
+                <h1 id="amount_label" style={{ height: "43px" }}>
+                  {isVisible ? (
+                    <CountUp
+                      start={0}
+                      end={box_number}
+                      duration={3}
+                      separator=","
+                    />
+                  ) : null}
+                </h1>
+              )}
+            </VisibilitySensor>
           </div>
         </div>
         <div
