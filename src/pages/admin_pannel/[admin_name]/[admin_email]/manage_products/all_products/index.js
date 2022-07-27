8c5a124bf21e582d7f42fcf8ea15466ed02b88e2
MHRafix <fmhrafiz27@gmail.com>
2022-07-27 11:00:58 +0600

    fixed

--- src/pages/admin_pannel/[admin_name]/[admin_email]/manage_products/all_products/index.js
+++ src/pages/admin_pannel/[admin_name]/[admin_email]/manage_products/all_products/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import AdminPannelLayoutContainer from "../../../../../../components/admin_pannel_components/common/layout/AdminPannelLayoutContainer";
 import AllProductsMain from "../../../../../../components/admin_pannel_components/components/manage_products/all_products/AllProductsMain";
 // import Order from "../../../../../../models/AllOrders";
-// import Product from "../../../../../../../models/Products";
+import AddProduct from "../../../../../../../models/PostProducts";
 // import User from "../../../../../../models/Users";
-// import db from "../../../../../../utilities/database";
+import db from "../../../../../../utilities/database";
 
 export default function AllProducts({ all_products }) {
   return (
@@ -19,36 +19,36 @@ export default function AllProducts({ all_products }) {
   );
 }
 
-// export async function getServerSideProps() {
-//   await db.connect();
-//   // const all_orders = await Order.find({});
-//   // const all_users = await User.find({});
-//   const all_products = await Product.find({});
-//   await db.disconnect();
-
-//   return {
-//     props: {
-//       // all_orders,
-//       // all_users,
-//       all_products,
-//     },
-//   };
-// }
-
 export async function getServerSideProps() {
-  // all orders
-  // const orders = await fetch(
-  //   `${process.env.ROOT_URI}/api/manage_orders/all_orders`
-  // );
-  // const all_orders = await orders.json();
+  await db.connect();
+  // const all_orders = await Order.find({});
+  // const all_users = await User.find({});
+  const all_products = await AddProduct.find({});
+  await db.disconnect();
 
-  // all products
-  const products = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
-  const all_products = await products.json();
-
-  // // all users
-  // const users = await fetch(`${process.env.ROOT_URI}/api/all_users`);
-  // const all_users = await users.json();
-
-  return { props: { all_products } };
+  return {
+    props: {
+      // all_orders,
+      // all_users,
+      all_products,
+    },
+  };
 }
+
+// export async function getServerSideProps() {
+//   // all orders
+//   // const orders = await fetch(
+//   //   `${process.env.ROOT_URI}/api/manage_orders/all_orders`
+//   // );
+//   // const all_orders = await orders.json();
+
+//   // all products
+//   const products = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
+//   const all_products = await products.json();
+
+//   // // all users
+//   // const users = await fetch(`${process.env.ROOT_URI}/api/all_users`);
+//   // const all_users = await users.json();
+
+//   return { props: { all_products } };
+// }
